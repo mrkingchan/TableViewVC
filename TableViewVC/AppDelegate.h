3236b4a787c9ba2adea3e3a69596c7032b6f38be
mrkingchan <1203710130@qq.com>
2018-04-27 14:51:27 +0800

    Initial Commit

---
+++ TableViewVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewVC
+//
+//  Created by Macx on 2018/4/27.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
